6ec15bc037bf6861ea914d37542617887bc8f2ff
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-12 10:02:56 +0100

    Dynamic routes examples

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,24 @@ Route::get('/', function () {
 Route::match(['get', 'post'], '/contacto', function() {
     return 'Hello World from contact page (GET or POST)';
 });
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// 1 variable
+Route::get('/products/{name}', function($name) {
+    return "Hello, product name: $name";
+});
+
+// 2 variables
+Route::get('/services/{name}/{category}', function($name, $category) {
+    return "Hello, service name: $name & category: $category";
+});
+
+// 1 variable mandatory & 1 optional
+Route::get('/experiences/{name}/{price?}', function($name, $price = null) {
+    if (is_numeric($price)) {
+        return "Hello, experience name: $name and his price is $price €";
+    } else {
+        return "Hello, experience name: $name";
+    }
+});
